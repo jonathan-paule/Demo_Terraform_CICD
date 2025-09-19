6f464bd93fbdf164cc5fd5317ba32becbfcc2e91
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-09-19 16:36:43 +0530

    Update main.tf

--- Terraform_workflow/env/dev/main.tf
+++ Terraform_workflow/env/dev/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-module "EC2_mod" {
+module "EC2_module" {
   source = "../../modules/EC_instance/" 
   vpc_id = "vpc-0306a6c9d13570470"
   ami= "ami-02d26659fd82cf299"
