e636fcaf8241fa4169e25082f127769ee26341e8
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-09-19 16:11:19 +0530

    Create main.tf

---
+++ Terraform_workflow/env/test/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "EC2_mod" {
+  source = "../../modules/EC_instance/" 
+  vpc_id= "vpc-0306a6c9d13570470"
+  ami= "ami-02d26659fd82cf299"
+
+
+}
+
