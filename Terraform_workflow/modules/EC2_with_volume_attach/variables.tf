9999cd6baa8ab68239d5ccceeb5d7f289099389b
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-09-19 12:04:02 +0530

    Create variables.tf

---
+++ Terraform_workflow/modules/EC2_with_volume_attach/variables.tf
@@ -0,0 +1,51 @@
+variable "key_name" {
+    description = "name of key pair"
+    type = string
+    default = "my_key_pair"
+  
+}
+
+variable "sg_name" {
+    description = "name of security group"
+    type = string
+    default = "ec2_sg"
+  
+}
+
+variable "vpc_id" {
+    description = "VPC ID where instance will be launched "
+    type = string
+      
+}
+variable "az" {
+    description = "AZ for EBS volume"
+    type = string
+  
+}
+
+variable "size" {
+    description = "size of ebs volume"
+    type = number
+    default = 1
+  
+}
+
+variable "instance_type" {
+    description = "EC2 instance type"
+    type = string
+    default = "t2.micro"
+
+  
+}
+variable "ami_id" {
+    description = "AMI id for the EC2 instance"
+    type = string
+  
+}
+
+variable "device_name" {
+    description = "device name for attaching ebs volume"
+    type = string
+    default = "/dev/sdh"
+  
+}
