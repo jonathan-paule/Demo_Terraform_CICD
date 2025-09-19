07a3a0b53c14c5a6fe3add3fd0bb6d95c3630c7f
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-09-19 17:34:05 +0530

    Update main.tf

--- Terraform_workflow/env/test/main.tf
+++ Terraform_workflow/env/test/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-module "EC2_mod" {
+module "EC2_mod2" {
   source = "../../modules/IAM_user_policy_attachment/"
   user_name= paul
   policy_arns = ["arn:aws:iam::aws:policy/AmazonS3FullAccess","arn:aws:iam::aws:policy/AmazonEC2FullAccess"]
