3c37eb11baac6d4579343b354b8aa3e2d8ecc58d
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-09-22 17:00:24 +0530

    Update main.tf

--- Terraform_workflow/env/test/main.tf
+++ Terraform_workflow/env/test/main.tf
@@ -3,13 +3,7 @@ provider "aws" {
 }
 
 module "EC2_mod" {
-  source = "../../modules/EC2_with_volume_attach"
-  az= "ap-south-1b"
-  ami_id= "ami-02d26659fd82cf299"
-  vpc_id= "vpc-0306a6c9d13570470"
-  key_name ="key2"
-  tag_name = "ec2_test"
-  sg_name = "test_sg"
-  env = "test"
+  source = "../../modules/s3_creation"
+  bucket_name= "hi123bucket978"
 
 }
