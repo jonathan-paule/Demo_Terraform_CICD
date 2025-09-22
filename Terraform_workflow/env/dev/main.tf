63e4ac38f0d8ec00e6e5f78c07a1793ab13a2dce
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-09-22 17:32:31 +0530

    Update main.tf

--- Terraform_workflow/env/dev/main.tf
+++ Terraform_workflow/env/dev/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "EC2_mod" {
   source = "../../modules/VPC"
-  cidr = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
   ami = "ami-02d26659fd82cf299"
   private_cidr ="10.0.2.0/24" 
   public_cidr= "10.0.1.0/24"
