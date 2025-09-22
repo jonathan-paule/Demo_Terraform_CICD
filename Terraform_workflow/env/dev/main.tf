51bc9395cd798640761500cb46bf3a262f062308
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-09-22 15:10:18 +0530

    Update main.tf

--- Terraform_workflow/env/dev/main.tf
+++ Terraform_workflow/env/dev/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "EC2_mod" {
-  source = "../../modules/EC2_with_volume_attach
+  source = "../../modules/EC2_with_volume_attach"
   az= "ap-south-1a"
   ami_id= "ami-02d26659fd82cf299"
   vpc_id= "vpc-0306a6c9d13570470"
