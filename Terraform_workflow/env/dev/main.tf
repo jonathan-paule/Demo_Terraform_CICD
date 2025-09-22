e8fdb5755f2d73fbcb8ecb7fb1dbcf7b380e9813
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-09-22 17:29:30 +0530

    Update main.tf

--- Terraform_workflow/env/dev/main.tf
+++ Terraform_workflow/env/dev/main.tf
@@ -3,8 +3,14 @@ provider "aws" {
 }
 
 module "EC2_mod" {
-  source = "../../modules/s3_creation"
-  bucket_name = "hello123bucket978rf"
+  source = "../../modules/VPC"
+  cidr = "10.0.0.0/16"
+  ami = "ami-02d26659fd82cf299"
+  private_cidr ="10.0.2.0/24" 
+  public_cidr= "10.0.1.0/24"
+  key_name = "vpc_key"
+  az = "ap-south-1a"
+
 
 
 
