db1de89d3b28b03a0bf4552b664e123cc30ba0b6
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-12 22:05:04 +0530

    Update main.tf

--- Terraform_workflow/modules/s3_creation/main.tf
+++ Terraform_workflow/modules/s3_creation/main.tf
@@ -14,6 +14,7 @@ resource "aws_s3_bucket_public_access_block" "access_good_1" {
    block_public_acls   = true
    block_public_policy = true
    restrict_public_buckets = true
+   ignore_public_acls=true
  }
 
  resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
@@ -114,6 +115,7 @@ resource "aws_s3_bucket_public_access_block" "access_good_2" {
    block_public_acls   = true
    block_public_policy = true
    restrict_public_buckets = true
+   ignore_public_acls=true
  }
 
 
