bf03c8c07394b17fb3212d14660c725d5232697a
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-12 12:56:15 +0530

    Update main.tf

--- Terraform_workflow/modules/s3_creation/main.tf
+++ Terraform_workflow/modules/s3_creation/main.tf
@@ -25,16 +25,37 @@ resource "aws_s3_bucket" "my_bucket" {
       expiration  {
         days = 90
     }
-         
+   }
+  
+    rule {
+
+      id = "log"
+      status = "Enabled"
+      filter{}
+      abort_incomplete_multipart_upload {
+       days_after_initiation = 7
+   }
+    
+  }
+ }
    
  
-   }
-}
+   
 
 resource "aws_s3_bucket" "log_bucket" {
   bucket = var.log_bucket
   
 }
+
+
+resource "aws_s3_bucket_versioning" "version1" {
+    bucket = aws_s3_bucket.log_bucket.id 
+
+    versioning_configuration {
+     status = "Enabled"
+   }
+ }
+
 
 resource "aws_s3_bucket_versioning" "version" {
     bucket = aws_s3_bucket.my_bucket.id 
@@ -54,5 +75,6 @@ resource "aws_s3_bucket_versioning" "version" {
    target_bucket = aws_s3_bucket.log_bucket.id
    target_prefix = "log/"
  }
+
 
 
