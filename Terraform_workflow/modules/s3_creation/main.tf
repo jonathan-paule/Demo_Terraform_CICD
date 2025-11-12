1079caa5583cbc72d8e37a30861a931f4d4300df
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-12 21:55:08 +0530

    Update main.tf

--- Terraform_workflow/modules/s3_creation/main.tf
+++ Terraform_workflow/modules/s3_creation/main.tf
@@ -13,8 +13,6 @@ resource "aws_s3_bucket_public_access_block" "access_good_1" {
 
    block_public_acls   = true
    block_public_policy = true
-   restrict_public_buckets = true
-   ignore_public_acls=true
  }
 
  resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
@@ -48,6 +46,22 @@ resource "aws_s3_bucket_public_access_block" "access_good_1" {
     
   }
  }
+
+resource "aws_sns_topic" "bucket_notifications" {
+  name = "bucket-notifications"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  topic {
+    topic_arn     = aws_sns_topic.bucket_notifications.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = "logs/"
+  }
+}
+
+
 
 resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
    bucket = aws_s3_bucket.log_bucket.id
@@ -98,9 +112,19 @@ resource "aws_s3_bucket_public_access_block" "access_good_2" {
 
    block_public_acls   = true
    block_public_policy = true
-   restrict_public_buckets = true
-   ignore_public_acls=true
  }
+
+
+
+resource "aws_s3_bucket_notification" "bucket_notification1" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  topic {
+    topic_arn     = aws_sns_topic.bucket_notifications.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = "logs/"
+  }
+}
 
 
 
