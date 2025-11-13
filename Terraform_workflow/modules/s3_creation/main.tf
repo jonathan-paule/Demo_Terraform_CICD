dda418ac03b77ff0dca28406877789478a47f31c
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-13 16:19:06 +0530

    Update main.tf

--- Terraform_workflow/modules/s3_creation/main.tf
+++ Terraform_workflow/modules/s3_creation/main.tf
@@ -8,6 +8,39 @@ resource "aws_s3_bucket" "my_bucket" {
 }
 
 
+resource "aws_kms_key" "mykey" {
+  description             = "KMS key for S3 bucket encryption"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "good_sse_1" {
+   bucket = aws_s3_bucket.my_bucket.bucket
+
+   rule {
+
+     apply_server_side_encryption_by_default {
+       kms_master_key_id = aws_kms_key.mykey.arn
+       sse_algorithm     = "aws:kms"
+     }
+   }
+ }
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "good_sse_2" {
+   bucket = aws_s3_bucket.log_bucket.bucket
+
+   rule {
+
+     apply_server_side_encryption_by_default {
+       kms_master_key_id = aws_kms_key.mykey.arn
+       sse_algorithm     = "aws:kms"
+     }
+   }
+ }
+
+
+
 resource "aws_s3_bucket_public_access_block" "access_good_1" {
    bucket = aws_s3_bucket.my_bucket.id
 
