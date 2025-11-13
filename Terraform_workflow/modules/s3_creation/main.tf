7fdeeb88812c77fb4d094661fb903258f1d50db0
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-13 17:47:44 +0530

    Update main.tf

--- Terraform_workflow/modules/s3_creation/main.tf
+++ Terraform_workflow/modules/s3_creation/main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
 }
 
-resource "aws_kms_key" "example" {
+resource "aws_kms_key" "mykey" {
   description = "KMS key "
   policy      = <<POLICY
   {
