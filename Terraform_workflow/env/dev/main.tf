9d5f05b6942f52a1b0c02107b6516708aaa93d25
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-13 17:03:13 +0530

    Update main.tf

--- Terraform_workflow/env/dev/main.tf
+++ Terraform_workflow/env/dev/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "EC2_mod" {
   source = "../../modules/s3_creation
   bucket_name= localbucket_77744_123
-  log_bucket= mylog976544274343322
+  log_bucket= mylog9765442743433409
   
 
 
