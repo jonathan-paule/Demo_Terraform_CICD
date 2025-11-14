0ded5de09933d1bb4f3d1257c48549ae00fcb455
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-14 11:27:57 +0530

    Update main.tf

--- Terraform_workflow/env/dev/main.tf
+++ Terraform_workflow/env/dev/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 module "EC2_mod" {
   source = "../../modules/s3_creation
   bucket_name= localbucket_77744_123
-  log_bucket= mylog976544274343347764
+  log_bucket= mylog9765442743433477676
   enable_key_rotation = true
 
   
