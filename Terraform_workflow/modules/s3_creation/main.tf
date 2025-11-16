e2723d7e24f8f6ff6aa581a77bfd1123964e9a3e
jonathan-paule <114561799+jonathan-paule@users.noreply.github.com>
2025-11-16 18:14:24 +0530

    Update main.tf

--- Terraform_workflow/modules/s3_creation/main.tf
+++ Terraform_workflow/modules/s3_creation/main.tf
@@ -3,12 +3,143 @@ provider "aws" {
   
 }
 
+provider "aws" {
+  alias = "east"
+  region = "us-east-1"
+  
+}
+
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_versioning" "version" {
+    bucket = aws_s3_bucket.my_bucket.id 
+
+    versioning_configuration {
+     status = "Enabled"
+   }
+ }
+
+ resource "aws_s3_bucket" "east_primary_bucket" {
+   provider = aws.east
+   bucket   = "tf-test-bucket-west-12345"
+ }
+
+ resource "aws_s3_bucket_versioning" "east" {
+   provider = aws.east
+
+   bucket = aws_s3_bucket.east_primary_bucket.id
+   versioning_configuration {
+     status = "Enabled"
+   }
+ }
+
+resource "aws_iam_role" "east_replication" {
+  name = "s3_cross_region_replication_role"
+
+  assume_role_policy = jsondecode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect= "Allow",
+      Principal = { Service = "s3.amazonaws.com" },
+      Action = "sts.Assume_Role"
+    }]
+})
+  
+}
+
+
+
+
+resource "aws_iam_role_policy" "east_replication_policy" {
+  role = aws_iam_role.east_replication.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObjectVersionForReplication",
+          "s3:GetObjectVersionAcl",
+          "s3:GetObjectVersionTagging",
+          "s3:ListBucket"
+        ],
+        Resource = [
+          aws_s3_bucket.my_bucket.arn,
+          "${aws_s3_bucket.my_bucket.arn}/*",
+          aws_s3_bucket.log_bucket.arn,
+          "${aws_s3_bucket.log_bucket.arn}/*"
+        ]
+      },
+
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:ReplicateObject",
+          "s3:ReplicateDelete",
+          "s3:ReplicateTags",
+          "s3:PutObjectAcl"
+        ],
+        Resource = [
+          aws_s3_bucket.east_primary_bucket.arn,
+          "${aws_s3_bucket.east_primary_bucket.arn}/*",
+          aws_s3_bucket.log_east_bucket.arn,
+          "${aws_s3_bucket.log_east_bucket.arn}/*"
+        ]
+      },
+
+  
+      {
+        Effect = "Allow",
+        Action = [
+          "kms:Decrypt",
+          "kms:DescribeKey"
+        ],
+        Resource = aws_kms_key.mykey.arn
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "kms:Encrypt",
+          "kms:GenerateDataKey",
+          "kms:DescribeKey"
+        ],
+        Resource = aws_kms_key.east_key.arn
+      }
+    ]
+  })
+}
+
+ resource "aws_s3_bucket_replication_configuration" "east_primary" {
+   depends_on = [aws_s3_bucket_versioning.version,aws_s3_bucket_versioning.east]
+   role   = aws_iam_role.east_replication.arn
+   bucket = aws_s3_bucket.my_bucket.id
+
+   rule {
+     status = "Enabled"
+
+     destination {
+       bucket        = aws_s3_bucket.east_primary_bucket.arn
+       storage_class = "STANDARD"
+     }
+     
+   }
+ }
+
+ resource "aws_s3_bucket" "log_bucket" {
+  bucket = var.log_bucket
+  
+}
+
+
+
 resource "aws_kms_key" "mykey" {
   description = "KMS key "
+  deletion_window_in_days = 10
+  is_enabled              = true
+  enable_key_rotation     = true
   policy      = <<POLICY
   {
     "Version": "2012-10-17",
@@ -28,15 +159,32 @@ resource "aws_kms_key" "mykey" {
 POLICY
 }
 
-
-
-
-resource "aws_kms_key" "mykey" {
-  description             = "KMS key for S3 bucket encryption"
-  deletion_window_in_days = 10
+resource "aws_kms_key" "mykey1" {
+  provider = aws.east
+  description = "KMS key1 "
+   deletion_window_in_days = 10
   is_enabled              = true
   enable_key_rotation     = true
+  policy      = <<POLICY
+  {
+    "Version": "2012-10-17",
+    "Id": "default",
+    "Statement": [
+      {
+        "Sid": "DefaultAllow",
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": "arn:aws:iam::251985278967:root"
+        },
+        "Action": "kms:*",
+        "Resource": "*"
+      }
+    ]
+  }
+POLICY
 }
+
+ 
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "good_sse_1" {
    bucket = aws_s3_bucket.my_bucket.bucket
@@ -124,7 +272,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 
 
 
-resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle1" {
    bucket = aws_s3_bucket.log_bucket.id
    rule {
     
@@ -162,10 +310,54 @@ resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
  
    
 
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = var.log_bucket
-  
-}
+
+resource "aws_s3_bucket_versioning" "version1" {
+    bucket = aws_s3_bucket.log_bucket.id 
+
+    versioning_configuration {
+     status = "Enabled"
+   }
+ }
+
+
+ resource "aws_s3_bucket" "log_east_bucket" {
+   provider = aws.east
+   bucket   = "tf-log-bucket-east-123456334"
+ }
+
+ resource "aws_s3_bucket_versioning" "log_east_versioning" {
+   provider = aws.east
+
+   bucket = aws_s3_bucket.log_east_bucket
+   versioning_configuration {
+     status = "Enabled"
+   }
+ }
+
+ resource "aws_s3_bucket_replication_configuration" "log_east" {
+   depends_on = [aws_s3_bucket_versioning.version1,aws_s3_bucket_versioning.log_east_versioning]
+   role   = aws_iam_role.east_replication.arn
+   bucket = aws_s3_bucket.log_bucket.id
+
+   rule {
+     status = "Enabled"
+
+     destination {
+       bucket        = aws_s3_bucket.log_east_bucket.arn
+       storage_class = "STANDARD"
+       access_control_translation {
+         owner = "Destination"
+       }
+       encryption_configuration {
+         replica_kms_key_id = aws_kms_key.mykey1
+       }
+     }
+     
+   }
+   
+   
+ }
+
 
 
 resource "aws_s3_bucket_public_access_block" "access_good_2" {
@@ -191,22 +383,9 @@ resource "aws_s3_bucket_notification" "bucket_notification1" {
 
 
 
-resource "aws_s3_bucket_versioning" "version1" {
-    bucket = aws_s3_bucket.log_bucket.id 
-
-    versioning_configuration {
-     status = "Enabled"
-   }
- }
 
 
-resource "aws_s3_bucket_versioning" "version" {
-    bucket = aws_s3_bucket.my_bucket.id 
 
-    versioning_configuration {
-     status = "Enabled"
-   }
- }
 
 
 
